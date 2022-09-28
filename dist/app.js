6575ffec58f1515514c2f231370be516055c6de0
shakoor123 <shakoorchachu2001@gmail.com>
2022-09-28 06:14:08 +0530

    simple app.js and error handle 404,

--- dist/app.js
+++ dist/app.js
@@ -1,2 +1,28 @@
 "use strict";
-console.log("oooooo");
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = __importDefault(require("express"));
+const http_errors_1 = __importDefault(require("http-errors"));
+const dotenv_1 = require("dotenv");
+(0, dotenv_1.config)();
+const app = (0, express_1.default)();
+app.get("/", (req, res, next) => {
+    res.send("home page");
+});
+app.use((req, res, next) => {
+    next(new http_errors_1.default.NotFound());
+});
+const errorHandler = (err, req, res, next) => {
+    res.status(err.status || 500);
+    res.send({
+        status: err.status || 500,
+        message: err.message,
+    });
+};
+app.use(errorHandler);
+const PORT = Number(process.env.PORT) || 3000;
+const server = app.listen(PORT, () => {
+    console.log("server running " + PORT);
+});
--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,35 @@
-console.log("oooooo")
+import express, {
+  Request,
+  Response,
+  NextFunction,
+  Application,
+  ErrorRequestHandler,
+} from "express";
+import { Server } from "http";
+import createHttpError from "http-errors";
+import { config } from "dotenv";
+
+config();
+const app: Application = express();
+
+app.get("/", (req: Request, res: Response, next: NextFunction) => {
+  res.send("home page");
+});
+
+app.use((req: Request, res: Response, next: NextFunction) => {
+  next(new createHttpError.NotFound());
+});
+
+const errorHandler: ErrorRequestHandler = (err, req, res, next) => {
+  res.status(err.status || 500);
+  res.send({
+    status: err.status || 500,
+    message: err.message,
+  });
+};
+
+app.use(errorHandler);
+const PORT: Number = Number(process.env.PORT) || 3000;
+const server: Server = app.listen(PORT, () => {
+  console.log("server running " + PORT);
+});
